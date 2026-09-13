2e821613e2362e032fbf7169d0c612061f1647db
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-13 04:07:53 -0400

    Route Discord Activity through browser runtime fix

--- src/discord-bots-entry.js
+++ src/discord-bots-entry.js
@@ -1,4 +1,4 @@
-import app, { ArenaCoordinator } from "./activity-clean-entry.js";
+import app, { ArenaCoordinator } from "./activity-browser-runtime-fix-entry.js";
 import { InteractionType, verifyDiscordRequest, interactionMessage, userFromInteraction } from "./discord.js";
 import { ensureSchema, loadActiveGameForChannel, saveGame } from "./storage.js";
 import { addFakeContestants } from "./core/simulation.js";
