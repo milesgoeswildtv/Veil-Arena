9108bfc604adafb49af738409e0f448688aeaef8
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-18 19:06:03 -0400

    Compose live visual preview adapter last

--- src/telegram-ui.js
+++ src/telegram-ui.js
@@ -3,11 +3,13 @@ import { applyTelegramFeaturePack } from "./telegram-feature-pack.js";
 import { applyTelegramPrizePack } from "./telegram-prize-pack.js";
 import { applyTelegramProductPass } from "./telegram-product-pass.js";
 import { injectVeilSfx } from "./sfx-integration.js";
+import { applyTelegramVisualEditorAdapter } from "./telegram-visual-editor-adapter.js";
 
 export function buildTelegramMiniAppHtml() {
   const baseHtml = telegramFxMiniAppHtml();
   const featureHtml = applyTelegramFeaturePack(baseHtml);
   const sponsorHtml = applyTelegramPrizePack(featureHtml);
   const audioHtml = injectVeilSfx(sponsorHtml);
-  return applyTelegramProductPass(audioHtml);
+  const productHtml = applyTelegramProductPass(audioHtml);
+  return applyTelegramVisualEditorAdapter(productHtml);
 }
