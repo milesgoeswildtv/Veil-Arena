6215c199c4541e2ea29f665ce53e94183fcc9041
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-18 10:28:24 -0400

    Bump Telegram build for round grid fix

--- src/worker.js
+++ src/worker.js
@@ -13,7 +13,7 @@ import { injectVeilSfx } from "./sfx-integration.js";
 export { ArenaCoordinator };
 
 const BASELINE = "2026-09-13-discord-activity-official-1";
-const TELEGRAM_BUILD = "2026-09-18-telegram-spectator-revival-1";
+const TELEGRAM_BUILD = "2026-09-18-telegram-round-grid-fix-1";
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data, null, 2), {
