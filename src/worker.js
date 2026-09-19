5bebcd4fcdb9dc757fe63288986764e33c1794c8
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-19 18:09:36 -0400

    Bump Telegram UI build marker

--- src/worker.js
+++ src/worker.js
@@ -7,7 +7,7 @@ import { handleDiscordActivityRoute } from "./discord-activity.js";
 export { ArenaCoordinator };
 
 const BASELINE = "2026-09-13-discord-activity-official-1";
-const TELEGRAM_BUILD = "2026-09-19-annotated-arena-layout-pass-1";
+const TELEGRAM_BUILD = "2026-09-19-compact-arena-logo-emoji-drawer-1";
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data, null, 2), {
